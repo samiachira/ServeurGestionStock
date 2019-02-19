8392f17955525a6fc9b1f909bf202f0e505e8380
samiachira <BenAliSamyia@yahoo.com>
2019-02-19 20:55:24 +0100

    first commit

---
+++ src/main/java/org/stock/ServeurGestionStockApplication.java
@@ -0,0 +1,13 @@
+package org.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockApplication.class, args);
+	}
+
+}
